fd35759228c9e0c38021690e09fbe84d5049d5ab
marin <marinpaunovic@gmail.com>
2022-11-25 17:23:49 +0100

    styling modifcations

--- src/modules/components/navbar/dropdownMenu.tsx
+++ src/modules/components/navbar/dropdownMenu.tsx
@@ -1,14 +1,31 @@
+import { RootState } from 'modules/redux/rootReducer';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export const DropdownMenu = (props: { menuToggle: string }) => {
+  const theme = useSelector((state: RootState) => state.theme.theme);
   const { menuToggle } = props;
   return (
     <div
-      className={`dropdown-menu`}
+      className={`dropdown-menu ${theme}`}
       style={menuToggle == 'open' ? { right: '0px' } : { right: '-215px' }}
     >
-      <Link to={'/login'}>login</Link>
-      <Link to={'/register'}>register</Link>
+      <Link
+        to={'/login'}
+        style={
+          theme === 'darkTheme' ? { color: 'whitesmoke' } : { color: 'black' }
+        }
+      >
+        login
+      </Link>
+      <Link
+        to={'/register'}
+        style={
+          theme === 'darkTheme' ? { color: 'whitesmoke' } : { color: 'black' }
+        }
+      >
+        register
+      </Link>
       <a>ide logout button</a>
     </div>
   );
