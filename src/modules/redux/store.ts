4dd01778851da363ef5f731695935a22febfd133
marin <marinpaunovic@gmail.com>
2023-01-09 16:35:08 +0100

    RootState interface removed,became unused

--- src/modules/redux/store.ts
+++ src/modules/redux/store.ts
@@ -2,19 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
-import { CoinsArray } from './coinList/coinListSlice';
-import { User } from 'firebase/auth';
-
-export interface RootState {
-  theme: { theme: string };
-  auth: { auth: User };
-  coinList: {
-    coinList: Array<CoinsArray>;
-    isAscending: boolean;
-    whatOrder: string;
-    howOrder: string;
-  };
-}
 
 const middlewares = [thunk];
 
