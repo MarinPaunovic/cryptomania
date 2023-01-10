4f7dc49dcfcfed2aba3418db9087208a1c3569e0
marin <marinpaunovic@gmail.com>
2023-01-10 12:28:32 +0100

    code readjusted to look cleaner

--- src/shared/functions/handleOrder.ts
+++ src/shared/functions/handleOrder.ts
@@ -1,51 +1,63 @@
 import { CoinsArray, CoinSlice } from 'modules/redux/coinList/coinListSlice';
 
+const defaultOrder = (sortedList: Array<CoinsArray>) => {
+  return sortedList.sort((i1, i2) => {
+    if (i1 && i2) {
+      if (i1.market_cap_rank > i2.market_cap_rank) {
+        return 1;
+      }
+      if (i1.market_cap_rank < i2.market_cap_rank) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  });
+};
+
+const ascendingOrderOfWhat = (sortedList: Array<CoinsArray>, whatOrder: string) => {
+  return sortedList.sort((i1, i2) => {
+    if (i1 && i2) {
+      if (i1[whatOrder] < i2[whatOrder]) {
+        return 1;
+      }
+      if (i1[whatOrder] > i2[whatOrder]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  });
+};
+
+const descendingOrderOfWhat = (sortedList: Array<CoinsArray>, whatOrder: string) => {
+  return sortedList.sort((i1, i2) => {
+    if (i1 && i2) {
+      if (i1[whatOrder] > i2[whatOrder]) {
+        return 1;
+      }
+      if (i1[whatOrder] < i2[whatOrder]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+    return 0;
+  });
+};
+
 export const handleOrder = ({ coinList, howOrder, whatOrder }: CoinSlice): Array<CoinsArray> => {
   let sortedList: Array<CoinsArray> = [...coinList];
 
   if (!whatOrder) {
-    sortedList = sortedList.sort((i1, i2) => {
-      if (i1 && i2) {
-        if (i1.market_cap_rank > i2.market_cap_rank) {
-          return 1;
-        }
-        if (i1.market_cap_rank < i2.market_cap_rank) {
-          return -1;
-        } else {
-          return 0;
-        }
-      }
-      return 0;
-    });
+    sortedList = defaultOrder(sortedList);
   }
   if (howOrder === 'asc') {
-    sortedList = sortedList.sort((i1, i2) => {
-      if (i1 && i2) {
-        if (i1[whatOrder] < i2[whatOrder]) {
-          return 1;
-        }
-        if (i1[whatOrder] > i2[whatOrder]) {
-          return -1;
-        } else {
-          return 0;
-        }
-      }
-      return 0;
-    });
+    sortedList = ascendingOrderOfWhat(sortedList, whatOrder);
   } else {
-    sortedList = sortedList.sort((i1, i2) => {
-      if (i1 && i2) {
-        if (i1[whatOrder] > i2[whatOrder]) {
-          return 1;
-        }
-        if (i1[whatOrder] < i2[whatOrder]) {
-          return -1;
-        } else {
-          return 0;
-        }
-      }
-      return 0;
-    });
+    sortedList = descendingOrderOfWhat(sortedList, whatOrder);
   }
   return sortedList;
 };
