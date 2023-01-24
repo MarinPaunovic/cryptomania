3bd39fe1521efd796dd93072afe0e7a38810d228
marin <marinpaunovic@gmail.com>
2023-01-24 10:21:57 +0100

    useAmount hook added

--- src/modules/portfolio/components/portfolio.tsx
+++ src/modules/portfolio/components/portfolio.tsx
@@ -1,7 +1,10 @@
 import { Watchlist } from 'modules/userProfile';
+import { useAmount } from '../hooks';
 import { Summary } from './summary';
 
 export const Portfolio = () => {
+  useAmount();
+
   return (
     <div>
       <Summary />
