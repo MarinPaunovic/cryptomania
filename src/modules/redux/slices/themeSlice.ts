95d84d620e36e877f2e0e9fa433bdc8c1719d93b
marin <marinpaunovic@gmail.com>
2023-01-24 11:11:58 +0100

    file location changed

---
+++ src/modules/redux/slices/themeSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState: {
+    theme: 'lightTheme',
+  },
+  reducers: {
+    toggle(state, action) {
+      state.theme = action.payload;
+    },
+  },
+});
+
+export const { toggle } = themeSlice.actions;
+export default themeSlice.reducer;
