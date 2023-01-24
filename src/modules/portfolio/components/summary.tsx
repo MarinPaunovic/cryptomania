41ea194b34ba990da172dbd20a085b5e0e1a6762
marin <marinpaunovic@gmail.com>
2023-01-24 10:25:39 +0100

    total balance added, holdings in preparation for adding

--- src/modules/portfolio/components/summary.tsx
+++ src/modules/portfolio/components/summary.tsx
@@ -1,8 +1,14 @@
+import { RootState } from 'modules/redux/rootReducer';
+import { useSelector } from 'react-redux';
+
 export const Summary = () => {
+  const { balance } = useSelector((state: RootState) => state.totalBalance);
+  const { holdings } = useSelector((state: RootState) => state.holdings);
+
   return (
     <div className="summary-wrapper">
       <div className="summary-content">
-        <div>total balance</div>
+        <div>total balance ${Math.floor(balance * 100) / 100}</div>
         <div>24h chance</div>
         <div>total profit loss</div>
       </div>
