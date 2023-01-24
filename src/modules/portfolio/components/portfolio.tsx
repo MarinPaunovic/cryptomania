8cbf79fcbff8494d29641ca4e206bd82e2c3d93d
marin <marinpaunovic@gmail.com>
2023-01-24 11:09:59 +0100

    cleanup for total balance added so total balance doesnt stack on re-enter

--- src/modules/portfolio/components/portfolio.tsx
+++ src/modules/portfolio/components/portfolio.tsx
@@ -1,9 +1,19 @@
 import { Watchlist } from 'modules/userProfile';
 import { useAmount } from '../hooks';
 import { Summary } from './summary';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setResetTotalBalance } from 'modules/redux/slices/totalBalance';
 
 export const Portfolio = () => {
+  const dispatch = useDispatch();
   useAmount();
+
+  useEffect(() => {
+    return () => {
+      dispatch(setResetTotalBalance());
+    };
+  }, [dispatch]);
 
   return (
     <div>
