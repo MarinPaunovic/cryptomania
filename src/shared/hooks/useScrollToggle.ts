2aab77d3c8aed9fdc97222342a4df40638a241cd
marin <marinpaunovic@gmail.com>
2023-01-24 10:57:07 +0100

    element prop added and passed accordingly, height added to styles

--- src/shared/hooks/useScrollToggle.ts
+++ src/shared/hooks/useScrollToggle.ts
@@ -1,22 +1,24 @@
 import { useEffect } from 'react';
 
-export const useScrollToggle = (isOpen: boolean) => {
+export const useScrollToggle = (isOpen: boolean, element: string) => {
   useEffect(() => {
+    const obj = document.getElementById(`${element}`);
     const styles = {
       position: 'fixed',
       top: '0',
       left: '0',
       right: '0',
       bottom: '0',
+      height: 'fit-content',
       'padding-right': '17px',
     };
+
     if (!isOpen) {
-      const obj = document.getElementById('homepage');
+      const obj = document.getElementById(`${element}`);
       if (obj) Object.assign(obj.style, { position: 'static', 'padding-right': '0px' });
       return;
     }
 
-    const obj = document.getElementById('homepage');
     if (obj) Object.assign(obj.style, styles);
-  }, [isOpen]);
+  }, [isOpen, element]);
 };
