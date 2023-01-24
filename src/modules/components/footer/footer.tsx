2f07d63c2581acc2131ff028044b070a22bd85ff
marin <marinpaunovic@gmail.com>
2023-01-24 17:09:14 +0100

    footer component classname naming changed to BEM same as footer css. Social networks links added + some little tweaks in desing

--- src/modules/components/footer/footer.tsx
+++ src/modules/components/footer/footer.tsx
@@ -1,23 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-wrapper main-align">
-        <div className="footer-title fc">
-          <div className="footer-title-title f">
-            <img src={require('images/cryptomania.jpg')} className="footer-title-image" />
-            <span className="footer-title-name">Cryptomania</span>
+      <div className="footer__wrapper g main-align">
+        <div className="footer__title fc">
+          <div className="footer__title-container f">
+            <img
+              src={require('images/cryptomania.jpg')}
+              alt="crypto"
+              className="footer__title-img"
+            />
+            <h1 className="footer__title-name">Cryptomania</h1>
           </div>
-          <div className="footer-title-description">
-            <p>
-              Cryptomania is learning project and also potential personal app for tracking crypto
-              world. <strong>Cryptomania is learning project</strong> and it's only for educational
-              purposes!
-            </p>
-          </div>
-          <p className="footer-title-copyright">© 2022 Cryptomania. All Rights Reserved.</p>
+          <p className="footer__description">
+            Cryptomania is learning project and also potential personal app for tracking crypto
+            world. Cryptomania is learning project and it's only for educational purposes!
+          </p>
+          <p className="footer__copyright">&copy; 2022 Cryptomania. All Rights Reserved.</p>
         </div>
-        <div className="footer-community">test</div>
-        <div className="footer-explore">test</div>
+        <div className="footer__explore f">
+          <p className="footer_explore-title"> Some useful content/links here </p>
+        </div>
+        <div className="footer__community fc aic">
+          <p className="footer__community-title ffam-content">
+            You can also check social media of coingecko
+          </p>
+          <div className="footer__social-links">
+            <a href="https://www.facebook.com/coingecko/" className="footer__social-btn">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a href="https://twitter.com/coingecko" className="footer__social-btn">
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            </a>
+            <a href="https://www.instagram.com/coingecko/" className="footer__social-btn">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="https://www.linkedin.com/company/coingecko/" className="footer__social-btn">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
