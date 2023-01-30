e0eff1348d3f9403ad81ec9672e834f6b2f5d332
marin <marinpaunovic@gmail.com>
2023-01-30 10:37:21 +0100

    code inside watchlist moved to hook for cleaner code

--- src/modules/userProfile/components/watchlist.tsx
+++ src/modules/userProfile/components/watchlist.tsx
@@ -1,37 +1,11 @@
-import { RootState } from 'modules/redux/rootReducer';
-import { useSelector } from 'react-redux';
-import { useFavorites } from 'shared';
-import { useEffect, useState } from 'react';
-import { CoinsArray } from 'modules/redux/slices/coinListSlice';
+import { useGetFavorites } from 'shared';
 import { useCoinList } from 'shared/hooks/useCoinList';
 import { Coin } from 'shared/components/coin';
 import { Description } from 'modules/homepage';
-import { handleOrder } from 'shared/functions/handleOrder';
 
 export const Watchlist: React.FC = () => {
-  const [favoriteList, setFavoriteList] = useState<Array<CoinsArray>>();
-  const { coinList, howOrder, whatOrder } = useSelector((state: RootState) => state.coinList);
-  const { favorites } = useFavorites();
   useCoinList();
-
-  useEffect(() => {
-    if (!coinList || !favorites.length) {
-      return;
-    }
-    const coins: Array<CoinsArray> = [];
-
-    favorites.map((item) => {
-      const coin = coinList.find((coin) => coin.name === item.name);
-      if (!coin) return;
-      coins.push(coin);
-    });
-    const sortedFavorites = handleOrder({
-      coinList: coins,
-      howOrder,
-      whatOrder,
-    });
-    setFavoriteList(sortedFavorites);
-  }, [coinList, favorites, howOrder, whatOrder]);
+  const { favoriteList } = useGetFavorites();
 
   return (
     <div className="watchlist ffam-content">
