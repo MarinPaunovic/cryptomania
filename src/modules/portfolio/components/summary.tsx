97a4d3abc94352012ccc68f9df19a32accc8c693
marin <marinpaunovic@gmail.com>
2023-02-06 10:51:25 +0100

    theme added

--- src/modules/portfolio/components/summary.tsx
+++ src/modules/portfolio/components/summary.tsx
@@ -1,18 +1,21 @@
+import { RootState } from 'modules/redux/rootReducer';
+import { useSelector } from 'react-redux';
 import { useOneDayChange, useTotalProfit } from '../hooks';
 import { useGetTotalBalance } from '../hooks/useGetTotalBalance';
 
 export const Summary = () => {
+  const { theme } = useSelector((state: RootState) => state.theme);
   const { totalProfit, profitRatio } = useTotalProfit();
   const { totalBalance } = useGetTotalBalance();
   const { price24h, ratio24h } = useOneDayChange();
 
   return (
     <div className="summary__wrapper fr ffam-content">
-      <div className="summary__box">
+      <div className={`summary__box ${theme}`}>
         <h3>{totalBalance == '' ? 'Loading..' : totalBalance}</h3>
         <span className="summary__box-title ffam-content">Total balance</span>
       </div>
-      <div className="summary__box">
+      <div className={`summary__box ${theme}`}>
         <h3>{price24h == '' ? 'Loading..' : price24h}</h3>
         <span className="summary__box-title ffam-content">
           24h change
@@ -25,7 +28,7 @@ export const Summary = () => {
           </span>
         </span>
       </div>
-      <div className="summary__box">
+      <div className={`summary__box ${theme}`}>
         <h3>{totalProfit == '' ? 'Loading..' : totalProfit}</h3>
         <div className="summary__box-title ffam-content">
           Total profit loss{' '}
