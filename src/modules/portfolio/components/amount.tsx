3ced5d828f5f1f79c46da5c78611d33d2055b495
marin <marinpaunovic@gmail.com>
2023-02-06 10:42:01 +0100

    utils removed from classname, classname added to list element, total split into 2 elements

--- src/modules/portfolio/components/amount.tsx
+++ src/modules/portfolio/components/amount.tsx
@@ -1,18 +1,20 @@
 import { AmountProps } from '../types';
 import { useCoinRatio } from '../hooks/useCoinRatio';
 import { useGetSingleValue } from '../hooks';
+import { useCurrencyConvert } from 'shared';
 
 export const Amount: React.FC<AmountProps> = ({ amount, tag, price }) => {
   const hookProps = { amount, price };
   const { singleValue: singleHoldingValue } = useGetSingleValue({ amount, price });
   const { ratio } = useCoinRatio(hookProps);
   const fiatValueFloor = singleHoldingValue && Math.floor(singleHoldingValue * 100) / 100;
+  const { currency } = useCurrencyConvert(fiatValueFloor);
 
   if (!amount)
     return (
-      <div className="portfolio-amount f asc jsc">
+      <div className="portfolio-amount">
         <ol className="fc">
-          <li className="fr">0.00$</li>
+          <li className="portfolio__amount-total fr">0.00$ (0%)</li>
           <li className="amount-holdings fr">
             0.0 <h4>{tag.toUpperCase()}</h4>
           </li>
@@ -21,10 +23,11 @@ export const Amount: React.FC<AmountProps> = ({ amount, tag, price }) => {
     );
 
   return (
-    <div className="portfolio-amount f asc jsc">
+    <div className="portfolio-amount">
       <ol className="fc">
-        <li className="fr">
-          {fiatValueFloor}$ ({ratio}%)
+        <li className="portfolio__amount-total fr">
+          <span>{currency}</span>
+          <span>({ratio}%)</span>
         </li>
         <li className="amount-holdings fr">
           {amount} <h4>{tag.toUpperCase()}</h4>
