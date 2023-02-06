6212f0e16c20400c7016ed726b21b1d90e86c66c
marin <marinpaunovic@gmail.com>
2023-02-06 11:02:34 +0100

    scroll sync added

--- src/modules/userProfile/components/watchlist.tsx
+++ src/modules/userProfile/components/watchlist.tsx
@@ -2,20 +2,27 @@ import { useGetFavorites } from 'shared';
 import { useCoinList } from 'shared/hooks/useCoinList';
 import { Coin } from 'shared/components/coin';
 import { Description } from 'modules/homepage';
+import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
 
 export const Watchlist: React.FC = () => {
   useCoinList();
   const { favoriteList } = useGetFavorites();
 
   return (
-    <div className="watchlist ffam-content">
-      <Description />
-      {favoriteList &&
-        favoriteList.map((item, i) => (
-          <div key={i}>
-            <Coin item={item} />
+    <ScrollSync>
+      <>
+        <Description />
+        <ScrollSyncPane>
+          <div className="watchlist ffam-content">
+            {favoriteList &&
+              favoriteList.map((item, i) => (
+                <div key={i}>
+                  <Coin item={item} />
+                </div>
+              ))}
           </div>
-        ))}
-    </div>
+        </ScrollSyncPane>
+      </>
+    </ScrollSync>
   );
 };
