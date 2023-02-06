7e7cd6422b2a2a54b3d6f91ba7d0443156f4b79b
marin <marinpaunovic@gmail.com>
2023-02-06 10:50:22 +0100

    favorites list added to handle empty list, empty list message added, ternary condition added for watchlist/porftolio

--- src/modules/portfolio/components/portfolioWrapper.tsx
+++ src/modules/portfolio/components/portfolioWrapper.tsx
@@ -3,14 +3,23 @@ import { RootState } from 'modules/redux/rootReducer';
 import { Watchlist } from 'modules/userProfile';
 import { useDispatch, useSelector } from 'react-redux';
 import { Portfolio } from './portfolio';
+import { useFavorites } from 'shared';
+import { Link } from 'react-router-dom';
 
 export const PortfolioWrapper = () => {
   const { active } = useSelector((state: RootState) => state.portfolio);
+  const { favorites } = useFavorites();
   const { theme } = useSelector((state: RootState) => state.theme);
   const dispatch = useDispatch();
 
   return (
-    <div className={`portfolio__wrapper main-align ${theme}`}>
+    <div
+      className={
+        active === 'watchlist'
+          ? `portfolio__wrapper portfolio__wrapper-watchlist main-align ${theme}`
+          : `portfolio__wrapper portfolio__wrapper-portfolio main-align ${theme}`
+      }
+    >
       <div className={`portfolio__actions fr main-align ${theme}`}>
         {active === 'watchlist' ? (
           <h1 className="portfolio__actions-title portfolio__actions-title--active ffam-content">
@@ -37,7 +46,21 @@ export const PortfolioWrapper = () => {
           </button>
         )}
       </div>
-      {active === 'watchlist' ? <Watchlist /> : <Portfolio />}
+      {active === 'watchlist' ? (
+        favorites.length ? (
+          <Watchlist />
+        ) : (
+          <h2 className="portfolio__favorites--empty ffam-content">
+            You don't have any coins added to favorites, go to{' '}
+            <Link to={'/'} className={`portfolio__favorites-link`}>
+              homepage
+            </Link>{' '}
+            to add some
+          </h2>
+        )
+      ) : (
+        <Portfolio />
+      )}
     </div>
   );
 };
