576a1e1a5ea2886bf8a704323495021473129264
marin <marinpaunovic@gmail.com>
2023-02-06 10:51:05 +0100

    theme added, classname added for labels

--- src/modules/portfolio/components/sellHoldings.tsx
+++ src/modules/portfolio/components/sellHoldings.tsx
@@ -13,6 +13,7 @@ export const SellHoldings: React.FC<HoldingsProps> = ({
 }) => {
   const [max, setMax] = useState<number>(0);
   const { holdings } = useSelector((state: RootState) => state.holdings);
+  const { theme } = useSelector((state: RootState) => state.theme);
 
   const handleMax = () => {
     const index = holdings.findIndex((holding) => holding.what === name);
@@ -24,15 +25,19 @@ export const SellHoldings: React.FC<HoldingsProps> = ({
   return (
     <form onSubmit={handleSubmit(onConfirm)}>
       <div className="modal-price">
-        <label htmlFor="price">Price per coin</label>
-        <input id="price" defaultValue={price} className="price-input" />
+        <label htmlFor="price" className="ffam-content">
+          Price per coin
+        </label>
+        <input id="price" defaultValue={price} className={`price-input ${theme}`} />
       </div>
       <div className="modal-amount">
-        <label htmlFor="amount">Amount</label>
+        <label htmlFor="amount" className="ffam-content">
+          Amount
+        </label>
         <div className="amount-input-wrapper">
           <input
             id="amount"
-            className="amount-input"
+            className={`amount-input ${theme}`}
             type="number"
             step={0.001}
             defaultValue={max > 0 ? max : undefined}
@@ -40,7 +45,7 @@ export const SellHoldings: React.FC<HoldingsProps> = ({
             onChange={(e) => setAmount(parseFloat(e.target.value))}
           ></input>
           <span className="amount-what">
-            <button onClick={handleMax} type="button">
+            <button onClick={handleMax} type="button" className={`${theme}`}>
               MAX
             </button>
             {tag.toLocaleUpperCase()}
