68735307cb78da17e3478763ce85612a95337381
marin <marinpaunovic@gmail.com>
2023-02-06 11:07:20 +0100

    code tweak due to scroll bugs and going to start of page height when allowing scroll,code added to check if mobile/pc to adjust padding

--- src/shared/hooks/useScrollToggle.ts
+++ src/shared/hooks/useScrollToggle.ts
@@ -2,23 +2,19 @@ import { useEffect } from 'react';
 
 export const useScrollToggle = (isOpen: boolean, element: string) => {
   useEffect(() => {
+    let padding = '0px';
+    if (
+      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    ) {
+      padding = '17px';
+    }
     const obj = document.getElementById(`${element}`);
-    const styles = {
-      position: 'fixed',
-      top: '0',
-      left: '0',
-      right: '0',
-      bottom: '0',
-      height: 'fit-content',
-      'padding-right': '17px',
-    };
-
     if (!isOpen) {
-      const obj = document.getElementById(`${element}`);
-      if (obj) Object.assign(obj.style, { position: 'static', 'padding-right': '0px' });
+      document.body.style.overflowY = 'auto';
+      if (obj) Object.assign(obj.style, { 'padding-right': '0px' });
       return;
     }
-
-    if (obj) Object.assign(obj.style, styles);
+    document.body.style.overflowY = 'hidden';
+    if (obj) Object.assign(obj.style, { 'padding-right': padding });
   }, [isOpen, element]);
 };
